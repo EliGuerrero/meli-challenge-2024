2aa9f70e993d639cb089a28e5dedc54dc9c6cb71
eliana.guerrero <elikateh@gmail.com>
2024-04-19 10:03:09 -0500

    Configuracion inicial del proyecto

---
+++ src/test/java/co/com/mercadolibre/challenge/seguridad/SeguridadApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.mercadolibre.challenge.seguridad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeguridadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
